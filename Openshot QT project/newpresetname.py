327dc8ae708c5d948079b09b3c2bd73e8d651514
olielvewen <olivier@openshot.org>
2018-12-28 18:45:45 +0100

    create the python file for New preset name file

---
+++ Openshot QT project/newpresetname.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#New Presset Name - A tool for the Openshot Qt project
+#Copyright (c) 2015 Olivier Girard <olivier@openshot.org>
+#
+#New Presset Name is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+#New Presset Name is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+import os
+
+from PyQt5.QtWidgets import *
+
+from newpresetnameui import Ui_Dialog
+
+class NewPresetName(QDialog):
+
+    def __init__(self, parent=None):
+        super(NewPresetName, self).__init__(parent)
+        self.setupUi()
+        self.connectActions()
+
+    #===================================================================================================================
+    def setupUi(self):
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+    #===================================================================================================================
+    def connectActions(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    presets = NewPresetName()
+    presets.show()
+    sys.exit(application.exec_())
